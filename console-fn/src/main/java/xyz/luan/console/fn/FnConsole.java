135de8adc69d2ca9b1c173d17612ebd11cc11c79
Luan Nico <luanpotter28@hotmail.com>
2014-10-03 13:41:20 -0300

    Project start

---
+++ console-fn/src/main/java/xyz/luan/console/fn/FnConsole.java
@@ -0,0 +1,84 @@
+package xyz.luan.console.fn;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+import java.io.IOException;
+
+import jline.console.ConsoleReader;
+
+import org.fusesource.jansi.Ansi.Color;
+
+import xyz.luan.console.parser.Console;
+
+public class FnConsole implements Console {
+	
+	private static ConsoleReader console;
+
+    public FnConsole() {
+        try {
+            console = new ConsoleReader();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	@Override
+    public void message(Object m) {
+        try {
+            console.println(ansi().fg(Color.GREEN).a(m).reset().toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	@Override
+    public void result(Object r) {
+        try {
+            console.print(ansi().fg(Color.GREEN).a("< ").reset().toString());
+            console.println(r.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+	@Override
+	public void error(Object e) {
+        try {
+            console.print(ansi().fg(Color.RED).a("< ").reset().toString());
+            console.println(e.toString());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+	}
+	
+	@Override
+    public String read() {
+        try {
+            final String readMessage = ansi().fg(Color.GREEN).a("> ").reset().toString();
+			return console.readLine(readMessage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
+
+	@Override
+	public char[] readPassword() {
+        try {
+        	final String readPassMessage = ansi().fg(Color.YELLOW).a("> ").reset().toString();
+        	return console.readLine(readPassMessage).toCharArray(); //TODO password
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+	}
+    
+	@Override
+    public void exit() {
+        try {
+            console.flush();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
---
+++ console-fn/src/main/java/xyz/luan/console/fn/FnSetup.java
@@ -0,0 +1,110 @@
+package xyz.luan.console.fn;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.reflections.Reflections;
+
+import xyz.luan.console.parser.Application;
+import xyz.luan.console.parser.Console;
+import xyz.luan.console.parser.Context;
+import xyz.luan.console.parser.Controller;
+import xyz.luan.console.parser.DefaultConsole;
+import xyz.luan.console.parser.Parser;
+import xyz.luan.console.parser.actions.InvalidAction;
+import xyz.luan.console.parser.actions.InvalidHandler;
+import xyz.luan.console.parser.call.Caller;
+import xyz.luan.console.parser.callable.Callable;
+import xyz.luan.console.parser.config.ConfigController;
+import xyz.luan.console.parser.config.HelpController;
+
+public abstract class FnSetup<T extends Context> {
+
+	private String controllersPackage;
+	
+	public FnSetup(String controllersPackage) {
+		this.controllersPackage = controllersPackage;
+	}
+	
+    public Application setupApplication(final T context) {
+        final Console console = new DefaultConsole();
+
+        Caller caller = defaultCaller(context, console);
+        Parser parser = getParser();
+        context.setup(parser, caller);
+
+        return createApplication(console, context);
+    }
+
+    /**
+     * Creates the custom application using the pre-built console and text
+     * @param console the console to be used
+     * @param context the context to be used
+     * @return the Application created
+     */
+    protected abstract Application createApplication(Console console, T context);
+    
+    /**
+     * Should read from file or return defaultParser()
+     * @return the parser
+     */
+	protected Parser getParser() {
+		return defaultParser();
+	}
+
+	/**
+	 * Should return a list of aliases
+	 * @return the list of aliases
+	 */
+	protected abstract Map<String, String> defaultAliases();
+
+    protected final Parser defaultParser() {
+        return new Parser(defaultAliases(), defaultCallables());
+    }
+
+    private Caller defaultCaller(T context, Console console) {
+    	Caller caller = new Caller();
+    	forEachController(c -> {
+			try {
+				Controller<T> controller = c.newInstance();
+				controller.setup(context, console);
+				caller.registerClass(c.getSimpleName(), controller);
+			} catch (IllegalAccessException | InvalidHandler | InvalidAction | InstantiationException e) {
+				throw new RuntimeException(e);
+			}
+    	});
+    	return caller;
+    }
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private void forEachController(Consumer<Class<? extends Controller<T>>> consumer) {
+		final Reflections reflections = new Reflections(controllersPackage);
+    	Set<Class<? extends Controller>> classes = reflections.getSubTypesOf(Controller.class);
+    	for (Class<? extends Controller> controller : classes) {
+    		consumer.accept((Class<? extends Controller<T>>) controller);
+    	}
+	}
+
+    private ArrayList<Callable> defaultCallables() {
+        ArrayList<Callable> callables = new ArrayList<>();
+
+        forEachController(c -> {
+			try {
+				Method defaultCallables = c.getMethod("defaultCallables", String.class, List.class);
+				defaultCallables.invoke(null, c.getSimpleName(), callables);
+			} catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+        });
+
+        ConfigController.defaultActions("config", callables);
+        HelpController.defaultActions("help", callables);
+
+        return callables;
+    }
+}
